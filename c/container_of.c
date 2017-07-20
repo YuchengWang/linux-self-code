146c00ce288b9f88ecb492fc3eb62a31bd51a26a
Yucheng Wang <yuchengwang@tsinghua.edu.cn>
2017-07-20 16:57:35 +0800

    modify contain fo

--- c/container_of.c
+++ c/container_of.c
@@ -30,22 +30,22 @@ int main(int argc, char* argv[]){
 	printf("%d %d\n", x, y);
 
 	struct s {
-		char m1;
-		char m2;
+		char j1;
+		char j2;
 	}s_t;
 	char *m1, *m2;
-	m1 = &(s_t.m1);
-	m2 = &(s_t.m2);
+	m1 = &(s_t.j1);
+	m2 = &(s_t.j2);
 
 	/* This will print 1 */
-	printf("m1 %d\n", &((struct s*)0)->m1);
-	printf("m2 %d\n", &((struct s*)0)->m2);
+	printf("m1 %d\n", &((struct s*)0)->j1);
+	printf("m2 %d\n", &((struct s*)0)->j2);
 
 	printf("s  addr:0x%08x\n", &s_t);
 	printf("m1 addr:0x%08x\n", m1);
 	printf("m2 addr:0x%08x\n", m2);
-	printf("m1 addr:0x%08x\n", container_of(m1, struct s, m1));
-	printf("m2 addr:0x%08x\n", container_of(m2, struct s, m2));
+	printf("m1 addr:0x%08x\n", container_of(m1, struct s, j1));
+	printf("m2 addr:0x%08x\n", container_of(m2, struct s, j2));
 
 
 	return 0;
